ac0b4660de7202b5a8fc9958ef73c80236e468bc
Tom Dörr <tomdoerr96@gmail.com>
2024-12-07 22:54:22 +0100

    Update project

--- plexsearch/plexsearch/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import perform_search
-
-__all__ = ['perform_search']
--- setup.py
+++
@@ -1,6 +0,0 @@
-# This file is kept for backwards compatibility with older tools.
-# The actual build configuration is in pyproject.toml
-
-from setuptools import setup
-
-setup()
